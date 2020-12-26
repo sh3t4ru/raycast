76c0adb5bc1f6f9e9790b2ab6628321db95afa95
sh3t4ru <76595990+sh3t4ru@users.noreply.github.com>
2020-12-26 03:35:19 +0300

    поправил растяжение сфер

--- painter.py
+++ painter.py
@@ -1,5 +1,7 @@
 from tkinter import Tk, Frame, BOTH, Canvas
 from rayengine import *
+import time
+
 
 class Example(Frame):
     def __init__(self, parent):
@@ -12,23 +14,28 @@ class Example(Frame):
         self.pack(fill=BOTH, expand=1)
 
         s1 = Sphere(Coordinate(100.0, 100.0, 50.0), "red", 40.0)
-        s2 = Sphere(Coordinate(130.0, 160.0, 40.0), "orange", 30.0)
+        s2 = Sphere(Coordinate(140.0, 140.0, 40.0), "orange", 40.0)
         s3 = Sphere(Coordinate(100.0, 70.0, 40.0), "blue", 20.0)
-        s4 = Sphere(Coordinate(120.0, 100.0, 30.0), "black", 25.0)
-
+        s4 = Sphere(Coordinate(120.0, 100.0, 38.0), "black", 29.0)
 
         canvas = Canvas(self)
-        for x in range(0, 300):
+
+        start_time = time.time()
+
+        for x in range(0, 200):
             for y in range(0, 200):
-                ray = Ray(Coordinate(150.0, 100.0, -50.0), Coordinate(x - 150.0, y - 100.0, 100.0))
+                ray = Ray(Coordinate(100.0, 100.0, -800.0), Coordinate(x - 100.0, y - 100.0, 800.0))
                 color = cast_ray(ray, [s1, s2, s3, s4])
                 canvas.create_line(x, y, x + 1, y + 1, fill=color)
 
+        print(time.time() - start_time)
+
         canvas.pack(fill=BOTH, expand=1)
+
 
 def main():
     height = 200
-    weight = 300
+    weight = 200
 
     root = Tk()
     root.geometry(str(weight) + "x" + str(height) + "+300+300")
